3108d7c8c0019cb0a311072507957e72f9f88b82
David Betteridge <david.betteridge@proactis.com>
2019-07-01 08:46:21 +0100

    Use unpinned package

--- Program.cs
+++ Program.cs
@@ -3,29 +3,30 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Markdig;
 
 namespace main_site
 {
-  class Program
-  {
-    static async Task Main(string[] args)
+    class Program
     {
-      var src = args.ElementAtOrDefault(0) ?? throw new Exception("src is required");
-      var dist = args.ElementAtOrDefault(1) ?? throw new Exception("dist is required");
+        static async Task Main(string[] args)
+        {
+            var src = args.ElementAtOrDefault(0) ?? throw new Exception("src is required");
+            var dist = args.ElementAtOrDefault(1) ?? throw new Exception("dist is required");
 
-      Directory.CreateDirectory(dist);
-      foreach (var file in System.IO.Directory.GetFiles(src, "*.md"))
-      {
-        var fileInfo = new FileInfo(file);
-        Console.WriteLine($"Converting {fileInfo.Name} to HTML");
+            var converter = new markdown_to_html.Converter();
 
-        var markdown = await File.ReadAllTextAsync(file);
+            Directory.CreateDirectory(dist);
+            foreach (var file in System.IO.Directory.GetFiles(src, "*.md"))
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"Converting {fileInfo.Name} to HTML");
 
-        var html = Markdown.ToHtml(markdown);
-        var htmlFilePath = Path.ChangeExtension(Path.Combine(dist, fileInfo.Name), "html");
-        await File.WriteAllTextAsync(htmlFilePath, html, Encoding.UTF8);
-      }
+                var markdown = await File.ReadAllTextAsync(file);
+
+                var html = converter.Execute(markdown);
+                var htmlFilePath = Path.ChangeExtension(Path.Combine(dist, fileInfo.Name), "html");
+                await File.WriteAllTextAsync(htmlFilePath, html, Encoding.UTF8);
+            }
+        }
     }
-  }
 }
