a6d1e9d528e5553647f3a1bcfb6556baffd52f74
David Betteridge <david.betteridge@proactis.com>
2019-07-01 21:02:07 +0100

    Add tests

---
+++ SomeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace main_site
+{
+    public class SomeTests
+    {
+        [Theory]
+        [ClassData(typeof(TestData))]
+        public void Test(int a)
+        {
+            Assert.True(a >= 0);
+        }
+    }
+
+    public class TestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            for (int i = 0; i < 118; i++)
+            {
+                yield return new object[] { i };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
